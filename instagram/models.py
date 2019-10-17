5a8b2a18e289d67c07b8943e141ac0a3f0a24ffb
UmuhireAnuarithe <anuarithemuhire@gmail.com.com>
2019-10-17 11:09:00 +0200

    profile model

--- instagram/models.py
+++ instagram/models.py
@@ -4,10 +4,20 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-from django.db import models
+
+class Profile(models.Model):
+    # photo = 
+    biography = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.biography
 
 class Image(models.Model):
+    # image = 
     title = models.CharField(max_length =30)
     caption = models.CharField(max_length =30)
     likes = models.IntegerField(default='none')
     comments = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.title
