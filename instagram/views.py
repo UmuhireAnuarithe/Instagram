53ba721f587be468ba9be91ae24934c93d8d0d6d
UmuhireAnuarithe <anuarithemuhire@gmail.com.com>
2019-10-17 11:34:51 +0200

    add changes

--- instagram/views.py
+++ instagram/views.py
@@ -2,7 +2,8 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http  import HttpResponse
-
+from .models import Profile,Image
 # Create your views here.
 def welcome(request):
-    return render(request,'welcome.html')
+    images = Image.objects.all()
+    return render(request,'welcome.html',{'images':images})
