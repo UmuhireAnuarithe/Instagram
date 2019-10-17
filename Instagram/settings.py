a8a986d0c824e902c0483d183bc4713c2a0da427
UmuhireAnuarithe <anuarithemuhire@gmail.com.com>
2019-10-17 10:30:44 +0200

    conected database

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -77,11 +77,12 @@ WSGI_APPLICATION = 'Instagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instagram',
+        'USER': 'wecode',
+    'PASSWORD':'12345',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
