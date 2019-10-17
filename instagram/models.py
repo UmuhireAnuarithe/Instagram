d3da46f73d07e7c8f3ec854b0e8a81925d07829c
UmuhireAnuarithe <anuarithemuhire@gmail.com.com>
2019-10-17 10:59:50 +0200

    image model

--- instagram/models.py
+++ instagram/models.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Image(models.Model):
+    title = models.CharField(max_length =30)
+    caption = models.CharField(max_length =30)
+    likes = models.IntegerField(default='none')
+    comments = models.CharField(max_length =30)
